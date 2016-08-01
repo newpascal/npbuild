328ce00025663a7b9a2173fc5c99f6738547e1f6
Maciej Izak <hnb.code@gmail.com>
2016-08-02 00:10:10 +0200

    Remove Generics.Collections from ccr

--- download4ccr.js
+++ download4ccr.js
@@ -9,11 +9,3 @@ download('https://github.com/newpascal-ccr/mORMot/zipball/heads/master').pipe(fs
         fs.renameSync('mORMot/' + fs.readdirSync('mORMot'), 'mORMot/latest'); 
       });
   });
-
-download('https://github.com/newpascal-ccr/generics.collections/zipball/heads/master').pipe(fs.createWriteStream('generics.collections.zip'))
-  .on('close', function () { 
-    fs.createReadStream('generics.collections.zip').pipe(unzip.Extract({ path: 'generics.collections' }))
-      .on('close', function () { 
-        fs.renameSync('generics.collections/' + fs.readdirSync('generics.collections'), 'generics.collections/latest'); 
-      });
-  });
