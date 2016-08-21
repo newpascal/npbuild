f4acb744398f165f93efe2504549ef2dc0933662
Maciej Izak <hnb.code@gmail.com>
2016-08-21 13:40:50 +0200

    Revert "try to fix problems on appveyor"
    This reverts commit ed00f67ccb48e6582f5b55bfe7b5af5c6365e750.

--- download4newpascalpack.js
+++ download4newpascalpack.js
@@ -13,16 +13,6 @@ const github = new GitHubApi({
     }
 });
 
-const github2 = new GitHubApi({
-    // optional
-    protocol: "https",
-    host: "api.github.com",
-    timeout: 5000,
-    headers: {
-        "user-agent": "NewPascal-App"
-    }
-});
-
 github.repos.getLatestRelease({user:'dathox',repo:'newpascal'},function(err,res){
   download(res.assets[0].browser_download_url).pipe(fs.createWriteStream('fpc.zip'))
   .on('close', function () {
@@ -34,7 +24,7 @@ github.repos.getLatestRelease({user:'dathox',repo:'newpascal'},function(err,res)
   });
 });
 
-github2.repos.getLatestRelease({user:'dathox',repo:'sparta'},function(err,res){
+github.repos.getLatestRelease({user:'dathox',repo:'sparta'},function(err,res){
   download(res.assets[0].browser_download_url).pipe(fs.createWriteStream('sparta.zip'))
   .on('close', function () {
     fs.createReadStream('sparta.zip').pipe(unzip.Extract({ path: 'newpascal' }));
