f594a016740fb14a5b6bfc4bca37e7adc0dd5cad
maciej-izak <hnb.code@gmail.com>
2017-03-01 00:42:31 +0100

    Download mORMot directly from upstream (ccr for mORMot is not needed anymore)

--- download4ccr.js
+++ download4ccr.js
@@ -2,7 +2,7 @@ const download = require('download');
 const fs = require('fs');
 const unzip = require('unzip');
 
-download('https://github.com/newpascal-ccr/mORMot/zipball/heads/master').pipe(fs.createWriteStream('mORMot.zip'))
+download('https://github.com/synopse/mORMot/zipball/heads/master').pipe(fs.createWriteStream('mORMot.zip'))
   .on('close', function () { 
     fs.createReadStream('mORMot.zip').pipe(unzip.Extract({ path: 'mORMot' }))
       .on('close', function () { 
